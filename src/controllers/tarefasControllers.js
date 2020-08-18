b7673b716d38ffbfc08f71dc4803f72bafff6fc3
Barrosbg <61169154+Barrosbg@users.noreply.github.com>
2020-08-18 19:27:41 -0300

    problema com funcao cath

--- src/controllers/tarefasControllers.js
+++ src/controllers/tarefasControllers.js
@@ -1,23 +1,17 @@
+const tarefa = require('../models/tarefa')
+
+const Tarefa = require('../models').Tarefa
+
 exports.ListAll = (req, res) => {
-    let tarefas = [
-        {
-            descricao: 'bla bla1',
-            data: '07/08/2020',
-            status: 'pendente1'
-        },
-        {
-            descricao: 'bla bla2',
-            data: '07/08/2020',
-            status: 'pendente2'
-        }
-    ]
-    res.send(tarefas)
+    Tarefa.findAll().then(tarefas => {
+        res.send(tarefas)
+    })
 }
 
 exports.createOne = (req, res) => {
-    let response = {
-        message: 'cadastrado',
-        data: req.body
-    }
-    res.send(response)
+    console.log(Tarefa)
+    const { descricao, datacria, status} = req.body
+    Tarefa.create({descricao, datacria, status}).then(tarefa => {
+        res.send(tarefa)
+    })
 }
